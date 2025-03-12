752c7c5e0d7ac88fd0ecebba59ecfa450aca6805
J-Sai-Jagadeesh <jagadeeshjsai@gmail.com>
2025-03-12 20:19:50 +0530

    Add files via upload

---
+++ denomination.py
@@ -0,0 +1,25 @@
+a=int(input("enter the amount:"))
+
+f500=a//500
+
+ra =a%500
+
+h100=ra//100
+
+ra = ra%100
+
+f50=ra//50
+
+ra = ra%50
+
+t10=ra//10
+
+ra = ra%10
+
+print (f"Five Hundred Notes (500):{f500}")
+
+print (f"Hundred Notes (100):{h100}")
+
+print (f"fifty Notes (50):{f50}")
+
+print (f"Ten Notes(10):{t10}")
